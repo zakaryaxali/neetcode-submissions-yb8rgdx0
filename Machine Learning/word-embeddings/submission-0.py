1d2bfe9b9f95d76ba754f7880a00e5ea7c30234e
Zakarya Ali <zakarya.ali@gmail.com>
2026-04-09 17:22:44 +0800

    Add: word-embeddings - submission-0

---
+++ Machine Learning/word-embeddings/submission-0.py
@@ -0,0 +1,15 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def lookup(self, embeddings: NDArray[np.float64], token_ids: NDArray[np.int64]) -> NDArray[np.float64]:
+        # embeddings: (vocab_size, embed_dim) matrix
+        # token_ids: 1D array of integer token IDs
+        # Return the embedding vectors for the given token IDs
+        # return np.round(your_answer, 5)
+        res = []
+        for token_id in token_ids:
+            res.append(embeddings[token_id])
+
+        return np.round(res, 5)
